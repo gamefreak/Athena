df67fc67a66400026d8eccdb97fece7d8c97311e
Scott McClaugherty <gamefreak4321@gmail.com>
2011-01-26 12:00:11 -0500

    Added LuaCoding category to NSString
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

---
+++ NSString+LuaCoding.h
@@ -0,0 +1,13 @@
+//
+//  NSString+LuaCoding.h
+//  Athena
+//
+//  Created by Scott McClaugherty on 1/26/11.
+//  Copyright 2011 Scott McClaugherty. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "LuaCoding.h"
+
+@interface NSString (LuaCoding) <LuaCoding>
+@end
---
+++ NSString+LuaCoding.m
@@ -0,0 +1,25 @@
+//
+//  NSString+LuaCoding.m
+//  Athena
+//
+//  Created by Scott McClaugherty on 1/26/11.
+//  Copyright 2011 Scott McClaugherty. All rights reserved.
+//
+
+#import "NSString+LuaCoding.h"
+#import "Archivers.h"
+
+@implementation NSString (LuaCoding)
+- (id) initWithLuaCoder:(LuaUnarchiver *)coder {
+    [self initWithString:[coder decodeString]];
+    return self;
+}
+
+- (void) encodeLuaWithCoder:(LuaArchiver *)coder {
+    [coder encodeString:self];
+}
+
++ (BOOL) isComposite {
+    return NO;
+}
+@end
