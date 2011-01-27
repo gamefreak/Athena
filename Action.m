d2be200dccc59f43ee4febe5abcde5f46e03d7ed
Scott McClaugherty <gamefreak4321@gmail.com>
2011-01-27 14:38:06 -0500

    Implemented typeForString: in Action.m
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- Action.m
+++ Action.m
@@ -56,4 +56,59 @@
 + (Class) classForLuaCoder:(LuaUnarchiver *)coder {
     return self;
 }
+
++ (ActionType) typeForString:(NSString *)type {
+    if ([type isEqual:@"none"]) {
+        return NoActionType;
+    } else if ([type isEqual:@"create object"]) {
+        return CreateObjectActionType;
+    } else if ([type isEqual:@"play sound"]) {
+        return PlaySoundActionType;
+    } else if ([type isEqual:@"alter"]) {
+        return AlterActionType;
+    } else if ([type isEqual:@"make sparks"]) {
+        return MakeSparksActionType;
+    } else if ([type isEqual:@"release energy"]) {
+        return ReleaseEnergyActionType;
+    } else if ([type isEqual:@"land at"]) {
+        return LandAtActionType;
+    } else if ([type isEqual:@"enter warp"]) {
+        return EnterWarpActionType;
+    } else if ([type isEqual:@"display message"]) {
+        return DisplayMessageActionType;
+    } else if ([type isEqual:@"change score"]) {
+        return ChangeScoreActionType;
+    } else if ([type isEqual:@"declare winner"]) {
+        return DeclareWinnerActionType;
+    } else if ([type isEqual:@"die"]) {
+        return DieActionType;
+    } else if ([type isEqual:@"set destination"]) {
+        return SetDestinationActionType;
+    } else if ([type isEqual:@"activate special"]) {
+        return ActivateSpecialActionType;
+    } else if ([type isEqual:@"activate pulse"]) {
+        return ActivatePulseActionType;
+    } else if ([type isEqual:@"activate beam"]) {
+        return ActivateBeamActionType;
+    } else if ([type isEqual:@"color flash"]) {
+        return ColorFlashActionType;
+    } else if ([type isEqual:@"create object set destination"]) {
+        return CreateObjectSetDestinationActionType;
+    } else if ([type isEqual:@"nil target"]) {
+        return NilTargetActionType;
+    } else if ([type isEqual:@"disable keys"]) {
+        return DisableKeysActionType;
+    } else if ([type isEqual:@"enable keys"]) {
+        return EnableKeysActionType;
+    } else if ([type isEqual:@"set zoom level"]) {
+        return SetZoomLevelActionType;
+    } else if ([type isEqual:@"computer select"]) {
+        return ComputerSelectActionType;
+    } else if ([type isEqual:@"assume initial object"]) {
+        return AssumeInitialObjectActionType;
+    } else {
+        @throw [NSString stringWithFormat:@"Unknown action type: %@", type];
+    }
+    return NoActionType;
+}
 @end
