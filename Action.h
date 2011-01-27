1db3ac92e2c015721d5323305e635835ff9bd9d8
Scott McClaugherty <gamefreak4321@gmail.com>
2011-01-27 14:24:07 -0500

    Added action type declarations.
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- Action.h
+++ Action.h
@@ -9,11 +9,71 @@
 #import <Cocoa/Cocoa.h>
 #import "LuaCoding.h"
 
+typedef enum {
+    NoActionType,
+    CreateObjectActionType,
+    PlaySoundActionType,
+    AlterActionType,
+    MakeSparksActionType,
+    ReleaseEnergyActionType,
+    LandAtActionType,
+    EnterWarpActionType,
+    DisplayMessageActionType,
+    ChangeScoreActionType,
+    DeclareWinnerActionType,
+    DieActionType,
+    SetDestinationActionType,
+    ActivateSpecialActionType,
+    ActivatePulseActionType,
+    ActivateBeamActionType,
+    ColorFlashActionType,
+    CreateObjectSetDestinationActionType,
+    NilTargetActionType,
+    DisableKeysActionType,
+    EnableKeysActionType,
+    SetZoomLevelActionType,
+    ComputerSelectActionType,
+    AssumeInitialObjectActionType
+} ActionType;
+
+typedef enum {
+    AlterHealth,
+    AlterVelocity,
+    AlterThrust,
+    AlterMaxThrust,
+    AlterMaxVelocity,
+    AlterMaxTurnRate,
+    AlterLocation,
+    AlterScale,
+    AlterPulseWeapon,
+    AlterBeamWeapon,
+    AlterSpecialWeapon,
+    AlterEnergy,
+    AlterOnwer,
+    AlterHidden,
+    AlterCloak,
+    AlterOffline,
+    AlterCurrentTurnRate,
+    AlterBaseType,
+    AlterActiveCondition,
+    AlterOccupation,
+    AlterAbsoluteCash,
+    AlterAge,
+    AlterAbsoluteLocation
+} AlterAction;
+
 @interface Action : NSObject <LuaCoding> {
+    ActionType type;
     BOOL reflexive;
     NSUInteger inclusiveFilter, exclusiveFilter;
     NSInteger subjectOverride, directOverride;
     NSInteger owner;
     NSInteger delay;
 }
++ (ActionType) typeForString:(NSString *)type;
++ (NSString *) stringForType:(ActionType) type;
 @end
+
+@interface NoAction : Action {}
+@end
+
