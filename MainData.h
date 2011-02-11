8b66578db2307920d276765e3aaddd1773e2ab02
Scott McClaugherty <gamefreak4321@gmail.com>
2011-02-10 19:47:39 -0500

    Added resource decoding and new properties to MainData.
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- MainData.h
+++ MainData.h
@@ -8,14 +8,58 @@
 
 #import <Cocoa/Cocoa.h>
 #import "LuaCoding.h"
+#import "ResCoding.h"
 
-@interface MainData : NSObject <LuaCoding> {
+#import "FlagBlob.h"
+
+@interface MainDataFlags : FlagBlob {
+    BOOL isNetworkable;
+    BOOL customObjects;
+    BOOL customRaces;
+    BOOL customScenarios;
+    BOOL isUnoptimized;
+}
+@property (readwrite) BOOL isNetworkable;
+@property (readwrite) BOOL customObjects;
+@property (readwrite) BOOL customRaces;
+@property (readwrite) BOOL customScenarios;
+@property (readwrite) BOOL isUnoptimized;
+@end
+
+@interface MainData : NSObject <LuaCoding, ResCoding> {
+    NSInteger inFlareId;
+    NSInteger outFlareId;
+    NSInteger playerBodyId;
+    NSInteger energyBlobId;
+    NSString *title;
+    NSString *downloadUrl;
+    NSString *author;
+    NSString *authorUrl;
+
+    MainDataFlags *flags;
+    NSUInteger version, minVersion;
+    NSUInteger checkSum;//BLAH
+
+
     NSMutableArray *objects;
     NSMutableArray *scenarios;
     NSMutableArray *races;
     NSMutableDictionary *sprites;
     NSMutableDictionary *sounds;
 }
+@property (readwrite) NSInteger inFlareId;
+@property (readwrite) NSInteger outFlareId;
+@property (readwrite) NSInteger playerBodyId;
+@property (readwrite) NSInteger energyBlobId;
+@property (readwrite, retain) NSString *title;
+@property (readwrite, retain) NSString *downloadUrl;
+@property (readwrite, retain) NSString *author;
+@property (readwrite, retain) NSString *authorUrl;
+@property (readonly) MainDataFlags *flags;
+@property (readwrite) NSUInteger version;
+@property (readwrite) NSUInteger minVersion;
+//@property (readonly) NSUInteger checkSum;
+
 @property (readonly) NSMutableArray *objects;
 @property (readonly) NSMutableArray *scenarios;
 @property (readonly) NSMutableArray *races;
--- MainData.m
+++ MainData.m
@@ -17,12 +17,41 @@
 #import "Condition.h"
 #import "Race.h"
 
+static NSArray *mainDataKeys;
+@implementation MainDataFlags
+@synthesize isNetworkable, customObjects, customRaces, customScenarios, isUnoptimized;
+
++ (NSArray *)keys {
+    if (mainDataKeys == nil) {
+        mainDataKeys = [[NSArray alloc] initWithObjects:@"isNetworkable", @"customObjects", @"customRaces", @"customScenarios", @"isUnoptimized", nil];
+    }
+    return mainDataKeys;
+}
+@end
+
+
 @implementation MainData
+@synthesize inFlareId, outFlareId, playerBodyId, energyBlobId;
+@synthesize title, downloadUrl, author, authorUrl, flags;
+@synthesize version, minVersion;
+//@synthesize checkSum;
+
 @synthesize objects, scenarios, races;
 @synthesize sprites, sounds;
 
 - (id) init {
     self = [super init];
+    playerBodyId = 22;
+    energyBlobId = 28;
+    inFlareId = 32;
+    outFlareId = 33;
+    title = @"";
+    downloadUrl = @"";
+    author = @"";
+    authorUrl = @"";
+
+    flags = [[MainDataFlags alloc] init];
+
     objects = [[NSMutableArray alloc] init];
     scenarios = [[NSMutableArray alloc] init];
     races = [[NSMutableArray alloc] init];
@@ -31,6 +60,7 @@
     return self;
 }
 
+//MARK: Lua Coding
 - (id) initWithLuaCoder:(LuaUnarchiver *)coder {
     self = [self init];
     [objects    setArray:[coder decodeArrayOfClass:[BaseObject class]      forKey:@"objects"    zeroIndexed:YES]];
@@ -38,23 +68,18 @@
     [races      setArray:[coder decodeArrayOfClass:[Race class]            forKey:@"race"       zeroIndexed:YES]];
     [sprites setDictionary:[coder decodeDictionaryOfClass:[NSString class] forKey:@"sprites"]];
     [sounds setDictionary:[coder decodeDictionaryOfClass:[NSString class] forKey:@"sounds"]];
+
+    [flags initWithLuaCoder:coder];
     return self;
 }
 
 - (void) encodeLuaWithCoder:(LuaArchiver *)coder {
+    [flags encodeLuaWithCoder:coder];
     [coder encodeArray:objects    forKey:@"objects"    zeroIndexed:YES];
     [coder encodeArray:scenarios  forKey:@"scenarios"  zeroIndexed:YES];
     [coder encodeArray:races      forKey:@"race"       zeroIndexed:YES];
     [coder encodeDictionary:sprites forKey:@"sprites" asArray:YES];
     [coder encodeDictionary:sounds forKey:@"sounds" asArray:YES];
-}
-
-
-- (void) dealloc {
-    [objects release];
-    [scenarios release];
-    [races release];
-    [super dealloc];
 }
 
 + (BOOL) isComposite {
@@ -63,5 +88,50 @@
 
 + (Class) classForLuaCoder:(LuaUnarchiver *)coder {
     return self;
+}
+
+//MARK: Res Coding
+- (id)initWithResArchiver:(ResUnarchiver *)coder {
+    self = [self init];
+    if (self) {
+        inFlareId = [coder decodeSInt32];
+        outFlareId = [coder decodeSInt32];
+        playerBodyId = [coder decodeSInt32];
+        energyBlobId = [coder decodeSInt32];
+
+        downloadUrl = [[coder decodePStringOfLength:0xff] retain];
+        title = [[coder decodePStringOfLength:0xff] retain];
+        author = [[coder decodePStringOfLength:0xff] retain];
+        authorUrl = [[coder decodePStringOfLength:0xff] retain];
+
+        version = [coder decodeUInt32];
+        minVersion = [coder decodeUInt32];
+
+        [flags initWithResArchiver:coder];
+        checkSum = [coder decodeUInt32];
+    }
+    return self;
+}
+
+//- (void)encodeResWithCoder:(ResArchiver *)archiver {
+//}
+
++ (ResType)resType {
+    return 'nlAG';
+}
+
++ (NSString *)typeKey {
+    return @"nlAG";
+}
+
++ (BOOL) isPacked {
+    return NO;
+}
+
+- (void) dealloc {
+    [objects release];
+    [scenarios release];
+    [races release];
+    [super dealloc];
 }
 @end
