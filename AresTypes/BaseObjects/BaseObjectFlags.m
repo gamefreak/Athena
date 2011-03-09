a49a9b4883882355bcde9776eb25b038729d6924
Scott McClaugherty <gamefreak4321@gmail.com>
2011-03-09 11:34:44 -0500

    Added names to BaseObjectFlags.

--- AresTypes/BaseObjects/BaseObjectFlags.m
+++ AresTypes/BaseObjects/BaseObjectFlags.m
@@ -8,7 +8,7 @@
 
 #import "BaseObjectFlags.h"
 
-static NSArray *bsobAttributeKeys;
+
 @implementation BaseObjectAttributes
 @synthesize canTurn, canBeEngaged, hasDirectionGoal,isRemote;
 @synthesize isHumanControlled;
@@ -21,6 +21,7 @@ static NSArray *bsobAttributeKeys;
 @synthesize appearOnRadar, onAutoPilot;
 
 + (NSArray *) keys {
+    static NSArray *bsobAttributeKeys;
     if (bsobAttributeKeys == nil) {
         bsobAttributeKeys = [[NSArray alloc] initWithObjects:@"canTurn", @"canBeEngaged", @"hasDirectionGoal", @"isRemote",
                              @"isHumanControlled", @"isBeam", @"doesBounce", @"isSelfAnimated",
@@ -34,9 +35,23 @@ static NSArray *bsobAttributeKeys;
     }
     return bsobAttributeKeys;
 }
+
++ (NSArray *) names {
+    static NSArray *bsobAttributeNames;
+    if (bsobAttributeNames == nil) {
+        bsobAttributeNames = [[NSArray alloc] initWithObjects:@"Can Turn", @"Can Be Engaged", @"Has Direction Goal", @"Is Remote",
+                             @"Is Human Controlled", @"Is Beam", @"Does Bounce", @"Is Self Animated",
+                             @"Shape From Direction", @"Is Player Ship", @"Can Be Destination", @"Can Engage",
+                             @"Can Evade", @"Can Accept Messages", @"Can Accept Build", @"Can Accept Destination",
+                             @"Auto Target", @"Animation Cycle", @"Can Collide", @"Can Be Hit",
+                             @"Is Destination", @"Hide Effect", @"Release Energy On Death", @"Hated",
+                             @"Occupies Space", @"Static Destination", @"Can Be Evaded", @"Neutral Death",
+                             @"Is Guided", @"Appears On Radar", [NSNull null], @"On Auto Pilot", nil];
+    }
+    return bsobAttributeNames;
+}
 @end
 
-static NSArray *bsobBuildFlags;
 @implementation BaseObjectBuildFlags
 @synthesize uncapturedBaseExists, sufficientEscortExists, thisBaseNeedsProtection;
 @synthesize friendUpTrend, friendDownTrend, foeUpTrend, foeDownTrend;
@@ -45,8 +60,9 @@ static NSArray *bsobBuildFlags;
 @synthesize levelKey1, levelKey2, levelKey3, levelKey4;
 
 + (NSArray *) keys {
-    if (bsobBuildFlags == nil) {
-        bsobBuildFlags = [[NSArray alloc] initWithObjects:
+    static NSArray *bsobBuildFlagsKeys;
+    if (bsobBuildFlagsKeys == nil) {
+        bsobBuildFlagsKeys = [[NSArray alloc] initWithObjects:
         @"uncapturedBaseExists", @"sufficientEscortExists", @"thisBaseNeedsProtection", @"friendUpTrend",
         @"friendDownTrend", @"foeUpTrend", @"foeDownTrend", @"matchingFoeExists",
         [NSNull null], [NSNull null], [NSNull null], [NSNull null],
@@ -56,11 +72,26 @@ static NSArray *bsobBuildFlags;
         @"engageKey1", @"engageKey2", @"engageKey3", @"engageKey4",
         @"levelKey1", @"levelKey2", @"levelKey3", @"levelKey4", nil];
     }
-    return bsobBuildFlags;
+    return bsobBuildFlagsKeys;
+}
+
++ (NSArray *) names {
+    static NSArray *bsobBuildFlagsNames;
+    if (bsobBuildFlagsNames == nil) {
+        bsobBuildFlagsNames = [[NSArray alloc] initWithObjects:
+                              @"Uncaptured Base Exists", @"Sufficient Escort Exists", @"This Base Needs Protection", @"Friend Up Trend",
+                              @"Friend Down Trend", @"Foe Up Trend", @"Foe Down Trend", @"Matching Foe Exists",
+                              [NSNull null], [NSNull null], [NSNull null], [NSNull null],
+                              [NSNull null], [NSNull null], [NSNull null], [NSNull null],
+                              [NSNull null], [NSNull null], [NSNull null], [NSNull null],
+                              [NSNull null], [NSNull null], @"Only Engaged By", @"Can Only Engage",
+                              @"Engage Key 1", @"Engage Key 2", @"Engage Key 3", @"Engage Key 4",
+                              @"Level Key 1", @"Level Key 2", @"Level Key 3", @"Level Key 4", nil];
+    }
+    return bsobBuildFlagsNames;
 }
 @end
 
-static NSArray *bsobOrderKeys;
 @implementation BaseObjectOrderFlags
 @synthesize strongerThanTarget, targetIsBase, targetIsNotBase, targetIsLocal, targetIsRemote;
 @synthesize onlyEscortNotBase, targetIsFriend, targetIsFoe, hardMatchingFriend, hardMatchingFoe;
@@ -69,8 +100,9 @@ static NSArray *bsobOrderKeys;
 @synthesize orderKey1, orderKey2, orderKey3, orderKey4;
 
 + (NSArray *) keys {
-    if (bsobOrderKeys == nil) {
-        bsobOrderKeys = [[NSArray alloc] initWithObjects:
+    static NSArray *bsobOrderKeyKeys;
+    if (bsobOrderKeyKeys == nil) {
+        bsobOrderKeyKeys = [[NSArray alloc] initWithObjects:
         @"strongerThanTarget", @"targetIsBase", @"targetIsNotBase", @"targetIsLocal",
         @"targetIsRemote", @"onlyEscortNotBase", @"targetIsFriend", @"targetIsFoe",
         [NSNull null], [NSNull null], [NSNull null], [NSNull null],
@@ -80,9 +112,24 @@ static NSArray *bsobOrderKeys;
         @"hardTargetIsFoe", @"hardTargetIsFriend", @"hardTargetIsNotBase", @"hardTargetIsBase",
         @"orderKey1", @"orderKey2", @"orderKey3", @"orderKey4", nil];
     }
-    return bsobOrderKeys;
+    return bsobOrderKeyKeys;
 }
 
++ (NSArray *) names {
+    static NSArray *bsobOrderKeyNames;
+    if (bsobOrderKeyNames == nil) {
+        bsobOrderKeyNames = [[NSArray alloc] initWithObjects:
+                            @"Stronger Than Target", @"Target Is Base", @"Target Is Not Base", @"Target Is Local",
+                            @"Target Is Remote", @"Only Escort Not Base", @"Target Is Friend", @"Target Is Foe",
+                            [NSNull null], [NSNull null], [NSNull null], [NSNull null],
+                            [NSNull null], [NSNull null], [NSNull null], [NSNull null],
+                            [NSNull null], [NSNull null], @"Hard Matching Friend", @"Hard Matching Foe",
+                            @"Hard Friendly Escort Only", @"Hard No Friendly Escort", @"Hard Target Is Remote", @"Hard Target Is Local",
+                            @"Hard Target Is Foe", @"Hard Target Is Friend", @"Hard Target Is Not Base", @"Hard Target Is Base",
+                            @"Order Key 1", @"Order Key 2", @"Order Key 3", @"Order Key 4", nil];
+    }
+    return bsobOrderKeyNames;
+}
 @end
 
 
