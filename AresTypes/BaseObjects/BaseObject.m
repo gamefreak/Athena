5215e76b6fd81710a7a2ed70973bb49e3f59f2ca
Scott McClaugherty <gamefreak4321@gmail.com>
2011-08-10 13:05:02 -0400

    Added if (self) wrapper to BaseObject's -initWithLuaCoder:

--- AresTypes/BaseObjects/BaseObject.m
+++ AresTypes/BaseObjects/BaseObject.m
@@ -136,135 +136,137 @@
 
 - (id) initWithLuaCoder:(LuaUnarchiver *)coder {
     self = [self init];
-    name = [[coder decodeStringForKey:@"name"] retain];
-    shortName = [[coder decodeStringForKey:@"shortName"] retain];
-    notes = [[coder decodeStringForKey:@"notes"] retain];
-    staticName = [[coder decodeStringForKey:@"staticName"] retain];
+    if (self) {
+        name = [[coder decodeStringForKey:@"name"] retain];
+        shortName = [[coder decodeStringForKey:@"shortName"] retain];
+        notes = [[coder decodeStringForKey:@"notes"] retain];
+        staticName = [[coder decodeStringForKey:@"staticName"] retain];
 
-    //disable the observation
-    [self removeObserver:self forKeyPath:@"objectType"];
-    [attributes release];
-    attributes = [[coder decodeObjectOfClass:[BaseObjectAttributes class] forKey:@"attributes"] retain];
-    [buildFlags release];
-    buildFlags = [[coder decodeObjectOfClass:[BaseObjectBuildFlags class] forKey:@"buildFlags"] retain];
-    [orderFlags release];
-    orderFlags = [[coder decodeObjectOfClass:[BaseObjectOrderFlags class] forKey:@"orderFlags"] retain];
+        //disable the observation
+        [self removeObserver:self forKeyPath:@"objectType"];
+        [attributes release];
+        attributes = [[coder decodeObjectOfClass:[BaseObjectAttributes class] forKey:@"attributes"] retain];
+        [buildFlags release];
+        buildFlags = [[coder decodeObjectOfClass:[BaseObjectBuildFlags class] forKey:@"buildFlags"] retain];
+        [orderFlags release];
+        orderFlags = [[coder decodeObjectOfClass:[BaseObjectOrderFlags class] forKey:@"orderFlags"] retain];
 
-    classNumber = [coder decodeIntegerForKey:@"class"];
-    race = [coder decodeIntegerForKey:@"race"];
+        classNumber = [coder decodeIntegerForKey:@"class"];
+        race = [coder decodeIntegerForKey:@"race"];
 
-    price = [coder decodeIntegerForKey:@"price"];
-    buildTime = [coder decodeIntegerForKey:@"buildTime"];
-    buildRatio = [coder decodeFloatForKey:@"buildRatio"];
+        price = [coder decodeIntegerForKey:@"price"];
+        buildTime = [coder decodeIntegerForKey:@"buildTime"];
+        buildRatio = [coder decodeFloatForKey:@"buildRatio"];
 
-    offence = [coder decodeFloatForKey:@"offence"];
-    escortRank = [coder decodeIntegerForKey:@"escortRank"];
+        offence = [coder decodeFloatForKey:@"offence"];
+        escortRank = [coder decodeIntegerForKey:@"escortRank"];
 
-    maxVelocity = [coder decodeFloatForKey:@"maxVelocity"];
-    warpSpeed = [coder decodeFloatForKey:@"warpSpeed"];
-    warpOutDistance = [coder decodeIntegerForKey:@"warpOutDistance"];
-    initialVelocity = [coder decodeFloatForKey:@"initialVelocity"];
-    initialVelocityRange = [coder decodeFloatForKey:@"initialVelocityRange"];
+        maxVelocity = [coder decodeFloatForKey:@"maxVelocity"];
+        warpSpeed = [coder decodeFloatForKey:@"warpSpeed"];
+        warpOutDistance = [coder decodeIntegerForKey:@"warpOutDistance"];
+        initialVelocity = [coder decodeFloatForKey:@"initialVelocity"];
+        initialVelocityRange = [coder decodeFloatForKey:@"initialVelocityRange"];
 
-    mass = [coder decodeFloatForKey:@"mass"];
-    thrust = [coder decodeFloatForKey:@"thrust"];
+        mass = [coder decodeFloatForKey:@"mass"];
+        thrust = [coder decodeFloatForKey:@"thrust"];
 
-    health = [coder decodeIntegerForKey:@"health"];
-    energy = [coder decodeIntegerForKey:@"energy"];
-    damage = [coder decodeIntegerForKey:@"damage"];
+        health = [coder decodeIntegerForKey:@"health"];
+        energy = [coder decodeIntegerForKey:@"energy"];
+        damage = [coder decodeIntegerForKey:@"damage"];
 
-    initialAge = [coder decodeIntegerForKey:@"initialAge"];
-    initialAgeRange = [coder decodeIntegerForKey:@"initialAgeRange"];
+        initialAge = [coder decodeIntegerForKey:@"initialAge"];
+        initialAgeRange = [coder decodeIntegerForKey:@"initialAgeRange"];
 
-    scale = [coder decodeFloatForKey:@"scale"];
-    layer = [coder decodeIntegerForKey:@"layer"];
-    spriteId = [coder decodeIntegerForKey:@"spriteId"];
+        scale = [coder decodeFloatForKey:@"scale"];
+        layer = [coder decodeIntegerForKey:@"layer"];
+        spriteId = [coder decodeIntegerForKey:@"spriteId"];
 
-    NSString *iconShapeStr = [coder decodeStringForKey:@"iconShape"];
-    if ([iconShapeStr isEqual:@"square"]){
-        iconShape = IconShapeSquare;
-    } else if ([iconShapeStr isEqual:@"triangle"]) {
-        iconShape = IconShapeTriangle;
-    } else if ([iconShapeStr isEqual:@"diamond"]) {
-        iconShape = IconShapeDiamond;
-    } else if ([iconShapeStr isEqual:@"plus"]) {
-        iconShape = IconShapePlus;
-    } else if ([iconShapeStr isEqual:@"framed square"]) {
-        iconShape = IconShapeFramedSquare;
-    } else if ([iconShapeStr isEqual:@"none"]){
-        //This case should only happen as a result of a fallback condition for the last case
-        iconShape = IconShapeNone;
-    } else {
-        iconShape = IconShapeNone;
+        NSString *iconShapeStr = [coder decodeStringForKey:@"iconShape"];
+        if ([iconShapeStr isEqual:@"square"]){
+            iconShape = IconShapeSquare;
+        } else if ([iconShapeStr isEqual:@"triangle"]) {
+            iconShape = IconShapeTriangle;
+        } else if ([iconShapeStr isEqual:@"diamond"]) {
+            iconShape = IconShapeDiamond;
+        } else if ([iconShapeStr isEqual:@"plus"]) {
+            iconShape = IconShapePlus;
+        } else if ([iconShapeStr isEqual:@"framed square"]) {
+            iconShape = IconShapeFramedSquare;
+        } else if ([iconShapeStr isEqual:@"none"]){
+            //This case should only happen as a result of a fallback condition for the last case
+            iconShape = IconShapeNone;
+        } else {
+            iconShape = IconShapeNone;
+        }
+
+        iconSize = [coder decodeIntegerForKey:@"iconSize"];
+
+        shieldColor = [coder decodeIntegerForKey:@"shieldColor"];
+
+        initialDirection = [coder decodeIntegerForKey:@"initialDirection"];
+        initialDirectionRange = [coder decodeIntegerForKey:@"initialDirectionRange"];
+
+        [weapons release];
+        weapons = [coder decodeDictionaryOfClass:[Weapon class] forKey:@"weapons"];
+        [weapons retain];
+
+        friendDefecit = [coder decodeFloatForKey:@"friendDefecit"];
+        dangerThreshold = [coder decodeFloatForKey:@"dangerThreshold"];
+
+    //    specialDirection = [coder decodeIntegerForKey:@"specialDirection"];
+
+        arriveActionDistance = [coder decodeIntegerForKey:@"arriveActionDistance"];
+
+        [actions setObject:[coder decodeObjectOfClass:[DestroyActionRef class]
+                                           forKeyPath:@"actions.destroy"]
+                    forKey:@"destroy"];
+
+        [actions setObject:[coder decodeObjectOfClass:[ActionRef class]
+                                           forKeyPath:@"actions.expire"]
+                    forKey:@"expire"];
+
+        [actions setObject:[coder decodeObjectOfClass:[ActionRef class]
+                                           forKeyPath:@"actions.create"]
+                    forKey:@"create"];
+
+        [actions setObject:[coder decodeObjectOfClass:[ActionRef class]
+                                           forKeyPath:@"actions.collide"]
+                    forKey:@"collide"];
+
+        [actions setObject:[coder decodeObjectOfClass:[ActivateActionRef class]
+                                           forKeyPath:@"actions.activate"]
+                    forKey:@"activate"];
+
+        [actions setObject:[coder decodeObjectOfClass:[ActionRef class]
+                                           forKeyPath:@"actions.arrive"]
+                    forKey:@"arrive"];
+
+        [frame release];
+        switch ([self objectType]) {
+            case RotationalObject:
+                frame = [coder decodeObjectOfClass:[RotationData class] forKey:@"rotation"];
+                break;
+            case AnimatedObject:
+                frame = [coder decodeObjectOfClass:[AnimationData class] forKey:@"animation"];
+                break;
+            case BeamObject:
+                frame = [coder decodeObjectOfClass:[BeamData class] forKey:@"beam"];
+                break;
+            case DeviceObject:
+                frame = [coder decodeObjectOfClass:[DeviceData class] forKey:@"device"];
+                break;
+        }
+        [frame retain];
+        //and re-enable
+        [self addObserver:self forKeyPath:@"objectType" options:NSKeyValueObservingOptionOld context:NULL];
+
+        skillNum = [coder decodeIntegerForKey:@"skillNum"];
+        skillDen = [coder decodeIntegerForKey:@"skillDen"];
+        skillNumAdj = [coder decodeIntegerForKey:@"skillNumAdj"];
+        skillNumAdj = [coder decodeIntegerForKey:@"skillDenAdj"];
+
+        portraitId = [coder decodeIntegerForKey:@"portraitId"];
     }
-
-    iconSize = [coder decodeIntegerForKey:@"iconSize"];
-
-    shieldColor = [coder decodeIntegerForKey:@"shieldColor"];
-
-    initialDirection = [coder decodeIntegerForKey:@"initialDirection"];
-    initialDirectionRange = [coder decodeIntegerForKey:@"initialDirectionRange"];
-
-    [weapons release];
-    weapons = [coder decodeDictionaryOfClass:[Weapon class] forKey:@"weapons"];
-    [weapons retain];
-
-    friendDefecit = [coder decodeFloatForKey:@"friendDefecit"];
-    dangerThreshold = [coder decodeFloatForKey:@"dangerThreshold"];
-
-//    specialDirection = [coder decodeIntegerForKey:@"specialDirection"];
-
-    arriveActionDistance = [coder decodeIntegerForKey:@"arriveActionDistance"];
-
-    [actions setObject:[coder decodeObjectOfClass:[DestroyActionRef class]
-                                       forKeyPath:@"actions.destroy"]
-                forKey:@"destroy"];
-
-    [actions setObject:[coder decodeObjectOfClass:[ActionRef class]
-                                       forKeyPath:@"actions.expire"]
-                forKey:@"expire"];
-
-    [actions setObject:[coder decodeObjectOfClass:[ActionRef class]
-                                       forKeyPath:@"actions.create"]
-                forKey:@"create"];
-
-    [actions setObject:[coder decodeObjectOfClass:[ActionRef class]
-                                       forKeyPath:@"actions.collide"]
-                forKey:@"collide"];
-
-    [actions setObject:[coder decodeObjectOfClass:[ActivateActionRef class]
-                                       forKeyPath:@"actions.activate"]
-                forKey:@"activate"];
-
-    [actions setObject:[coder decodeObjectOfClass:[ActionRef class]
-                                       forKeyPath:@"actions.arrive"]
-                forKey:@"arrive"];
-
-    [frame release];
-    switch ([self objectType]) {
-        case RotationalObject:
-            frame = [coder decodeObjectOfClass:[RotationData class] forKey:@"rotation"];
-            break;
-        case AnimatedObject:
-            frame = [coder decodeObjectOfClass:[AnimationData class] forKey:@"animation"];
-            break;
-        case BeamObject:
-            frame = [coder decodeObjectOfClass:[BeamData class] forKey:@"beam"];
-            break;
-        case DeviceObject:
-            frame = [coder decodeObjectOfClass:[DeviceData class] forKey:@"device"];
-            break;
-    }
-    [frame retain];
-    //and re-enable
-    [self addObserver:self forKeyPath:@"objectType" options:NSKeyValueObservingOptionOld context:NULL];
-
-    skillNum = [coder decodeIntegerForKey:@"skillNum"];
-    skillDen = [coder decodeIntegerForKey:@"skillDen"];
-    skillNumAdj = [coder decodeIntegerForKey:@"skillNumAdj"];
-    skillNumAdj = [coder decodeIntegerForKey:@"skillDenAdj"];
-
-    portraitId = [coder decodeIntegerForKey:@"portraitId"];
     return self;
 }
 
