ddc635a7bade70371aed98f3b4e6e75b3750dc9d
Scott McClaugherty <gamefreak4321@gmail.com>
2011-09-13 19:06:01 -0400

    Added format conversion from command line.
    Data downloads from command line are broken.

--- main.m
+++ main.m
@@ -8,6 +8,8 @@
 
 #import <Cocoa/Cocoa.h>
 #import "cmdline.h"
+#import "ApplicationDelagate.h"
+#import "AthenaDocument.h"
 
 int main(int argc, char *argv[]) {
     struct athena_args args;
@@ -23,26 +25,87 @@ int main(int argc, char *argv[]) {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
     //Determine file to work with.
-    //Get desired path in a NSString
+    //Get desired path
+    NSString *path = nil;
+    BOOL needsToDownloadData = NO;
     if (args.base_arg == NULL) {
         //use downloaded info from Application Support
+        path = [ApplicationDelagate supportDir];
+        [ApplicationDelagate ensureDirectoryExists:path];
+        path = [path stringByAppendingPathComponent:@"Ares Data"];
+        path = [path stringByAppendingPathComponent:@"Ares Scenarios"];
+        //check if we need to download anyway
+        needsToDownloadData = ![[NSUserDefaults standardUserDefaults] boolForKey:XSHasAresData];
     } else {
         //use specified file
+        path = [NSString stringWithUTF8String:args.base_arg];
+    }
+    if ((args.download_given && args.base_arg == NULL) || needsToDownloadData) {
+        //Download data from host to desired file path
+        NSTask *task;
+        //Download
+        task = [[[NSTask alloc] init] autorelease];
+        [task setLaunchPath:@"/usr/bin/curl"];
+        [task setArguments:[NSArray arrayWithObjects:@"-O", XSAresDataUrl, nil]];
+        [task setCurrentDirectoryPath:[ApplicationDelagate supportDir]];
+        [task launch];
+        [task waitUntilExit];
+        if ([task terminationStatus]) exit(1);
+        //And unzip
+        task = [[[NSTask alloc] init] autorelease];
+        [task setLaunchPath:@"/usr/bin/ditto"];
+        [task setArguments:[NSArray arrayWithObjects:@"-xk", [[ApplicationDelagate supportDir] stringByAppendingPathComponent:@"AresMedia.zip"], [[ApplicationDelagate supportDir] stringByAppendingPathComponent:@"Ares Data"], nil]];
+        [task setCurrentDirectoryPath:[ApplicationDelagate supportDir]];
+        [task launch];
+        [task waitUntilExit];
+        if ([task terminationStatus]) exit(1);
     }
 
-    if (args.download_given) {
-        //Download data from host to desired file path
+    NSURL *url = [NSURL fileURLWithPath:path];
+    NSError *error = nil;
+    NSString *type = [[NSWorkspace sharedWorkspace] typeOfFile:path error:&error];
+    if (error) {
+        NSLog(@"ERROR %@", error);
+        exit(1);
+    }
+
+    AthenaDocument *document = [[[AthenaDocument alloc] initWithContentsOfURL:url ofType:type error:&error] autorelease];
+    if (error) {
+        NSLog(@"ERROR %@", error);
+        exit(1);
     }
 
     //FUTURE: handle check
     //FUTURE: handle list
 
-    if (args.type_given) {
-        assert(args.out_given);
+    //Should we create a new file?
+    if (args.out_given) {
+        assert(args.type_given);
         //retrieve output type
-        //retrieve output filename
+        NSString *type = nil;
+        switch (args.type_arg) {
+            case type_arg_ares:
+                type = @"com.biggerplanet.AresData";
+                break;
+            case type_arg_lua:
+                type = @"org.brainpen.XseraLua";
+                break;
+            case type_arg_xsera:
+                type = @"org.brainpen.XseraData";
+            default:
+                assert(0);
+                break;
+        }
+        assert(type);
+        //Get output path
+        NSURL *outUrl = [NSURL fileURLWithPath:[NSString stringWithUTF8String:args.out_arg]];
         //save file
+        BOOL result = [document writeToURL:outUrl ofType:type error:&error];
+        if (!result || error) {
+            exit(1);
+        }
     }
+    //clean up
     [pool drain];
     arg_parser_free(&args);
     return 0;
