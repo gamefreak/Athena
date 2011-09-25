712d3739f77fe52c4345dfc650911f8ec0296fec
Scott McClaugherty <gamefreak4321@gmail.com>
2011-09-25 15:07:09 -0400

    Relevant methods on LuaArchiver are now synchronized for protection.

--- Archivers/Lua/LuaArchiver.m
+++ Archivers/Lua/LuaArchiver.m
@@ -20,8 +20,10 @@
 
 @implementation LuaArchiver (Private)
 - (void) indent {
-    int depth = [keyStack count];
-    for (int i = 1; i < depth; i++) [data appendString:@"\t"];
+    @synchronized(files) {
+        int depth = [keyStack count];
+        for (int i = 1; i < depth; i++) [data appendString:@"\t"];
+    }
 }
 @end
 
@@ -43,13 +45,15 @@
 }
 
 - (void) dealloc {
-    dispatch_group_wait(disp_group, DISPATCH_TIME_FOREVER);
-    dispatch_release(disp_group);
-    dispatch_release(disp_queue);
-    [files release];
-    [keyStack release];
-    [data release];
-    [super dealloc];
+    @synchronized(self) {
+        dispatch_group_wait(disp_group, DISPATCH_TIME_FOREVER);
+        dispatch_release(disp_group);
+        dispatch_release(disp_queue);
+        [files release];
+        [keyStack release];
+        [data release];
+        [super dealloc];
+    }
 }
 
 
@@ -58,8 +62,10 @@
 }
 
 - (NSData *) data {
-    if ([keyStack count] > 0) @throw @"Stack not empty";
-    return [data dataUsingEncoding:NSUTF8StringEncoding];
+    @synchronized(files) {
+        if ([keyStack count] > 0) @throw @"Stack not empty";
+        return [data dataUsingEncoding:NSUTF8StringEncoding];
+    }
 }
 
 
@@ -80,123 +86,143 @@
 }
 
 - (void) encodeObject:(id <NSObject, LuaCoding>)obj forKey:(NSString *)key {
-    [keyStack addObject:key];
-    [self indent];
-    [data appendString:key];
-    if ([[obj class] isComposite]) {
-        [data appendString:@" = {\n"];
-    } else {
-        [data appendString:@" = "];
-    }
-    [obj encodeLuaWithCoder:self];
-    if ([[obj class] isComposite]) {
+    @synchronized(self) {
+        [keyStack addObject:key];
         [self indent];
-        [data appendString:@"};\n"];
-    } else {
-        [data appendString:@";\n"];
+        [data appendString:key];
+        if ([[obj class] isComposite]) {
+            [data appendString:@" = {\n"];
+        } else {
+            [data appendString:@" = "];
+        }
+        [obj encodeLuaWithCoder:self];
+        if ([[obj class] isComposite]) {
+            [self indent];
+            [data appendString:@"};\n"];
+        } else {
+            [data appendString:@";\n"];
+        }
+        [keyStack removeLastObject];
     }
-    [keyStack removeLastObject];
 }
 
 - (void) encodeArray:(NSArray *)array forKey:(NSString *)key zeroIndexed:(BOOL)isZeroIndexed {
-    [keyStack addObject:key];
-    [self indent];
-    [data appendString:key];
-    [data appendString:@" = {\n"];
-    NSInteger idx = (isZeroIndexed?0:1);
-    for (id<LuaCoding> obj in array) {
-        [self encodeObject:obj
-                    forKey:[NSString stringWithFormat:@"[%d]", idx]];
-        idx++;
+    @synchronized(self) {
+        [keyStack addObject:key];
+        [self indent];
+        [data appendString:key];
+        [data appendString:@" = {\n"];
+        NSInteger idx = (isZeroIndexed?0:1);
+        for (id<LuaCoding> obj in array) {
+            [self encodeObject:obj
+                        forKey:[NSString stringWithFormat:@"[%d]", idx]];
+            idx++;
+        }
+        [self indent];
+        [data appendString:@"};\n"];
+        [keyStack removeLastObject];
     }
-    [self indent];
-    [data appendString:@"};\n"];
-    [keyStack removeLastObject];
 }
 
 - (void) encodeDictionary:(NSDictionary *)dict forKey:(NSString *)key asArray:(BOOL)asArray {
-    [keyStack addObject:key];
-    [self indent];
-    [data appendString:key];
-    [data appendString:@" = {\n"];
-    //Alphabetical sort
-    NSArray *keys = [[dict allKeys] sortedArrayUsingSelector:@selector(compare:)];
-    for (id key in keys) {
-        id<LuaCoding> obj = [dict objectForKey:key];
-        if (asArray) {
-            [self encodeObject:obj forKey:[NSString stringWithFormat:@"[%@]", key]];
-        } else {
-            [self encodeObject:obj forKey:key];
+    @synchronized(self) {
+        [keyStack addObject:key];
+        [self indent];
+        [data appendString:key];
+        [data appendString:@" = {\n"];
+        //Alphabetical sort
+        NSArray *keys = [[dict allKeys] sortedArrayUsingSelector:@selector(compare:)];
+        for (id key in keys) {
+            id<LuaCoding> obj = [dict objectForKey:key];
+            if (asArray) {
+                [self encodeObject:obj forKey:[NSString stringWithFormat:@"[%@]", key]];
+            } else {
+                [self encodeObject:obj forKey:key];
+            }
         }
+        [self indent];
+        [data appendString:@"};\n"];
+        [keyStack removeLastObject];
     }
-    [self indent];
-    [data appendString:@"};\n"];
-    [keyStack removeLastObject];
 }
 
 - (void) encodeBool:(BOOL)value {
-    [data appendString:(value?@"true":@"false")];
+    @synchronized(self) {
+        [data appendString:(value?@"true":@"false")];
+    }
 }
 
 - (void) encodeBool:(BOOL)value forKey:(NSString *)key {
-    [keyStack addObject:key];
-    [self indent];
-    [data appendFormat:@"%@ = %@;\n", key, (value?@"true":@"false")];
-    [keyStack removeLastObject];
+    @synchronized(self) {
+        [keyStack addObject:key];
+        [self indent];
+        [data appendFormat:@"%@ = %@;\n", key, (value?@"true":@"false")];
+        [keyStack removeLastObject];
+    }
 }
 
 - (void) encodeString:(NSString *)string {
-    NSMutableString *str = [NSMutableString stringWithString:string];
-    NSRange range = [str rangeOfString:@"]"];
-    NSMutableString *buffer = [NSMutableString string];
-    while (range.location != NSNotFound) {
-        [buffer appendString:@"="];
-        range = [str rangeOfString:[NSString stringWithFormat:@"]%@]", buffer]];
-    }
+    @synchronized(self) {
+        NSMutableString *str = [NSMutableString stringWithString:string];
+        NSRange range = [str rangeOfString:@"]"];
+        NSMutableString *buffer = [NSMutableString string];
+        while (range.location != NSNotFound) {
+            [buffer appendString:@"="];
+            range = [str rangeOfString:[NSString stringWithFormat:@"]%@]", buffer]];
+        }
 
-    [data appendFormat:@"[%@[%@]%@]", buffer, str, buffer];
+        [data appendFormat:@"[%@[%@]%@]", buffer, str, buffer];
+    }
 }
 
 - (void) encodeString:(NSString *)string forKey:(NSString *)key {
-    [keyStack addObject:key];
-    [self indent];
-    [data appendFormat:@"%@ = ", key];
-    [self encodeString:string];
-    [data appendString:@";\n"];
-    [keyStack removeLastObject];
+    @synchronized(self) {
+        [keyStack addObject:key];
+        [self indent];
+        [data appendFormat:@"%@ = ", key];
+        [self encodeString:string];
+        [data appendString:@";\n"];
+        [keyStack removeLastObject];
+    }
 }
 
 - (void) encodeFloat:(float)value forKey:(NSString *)key {
-    [keyStack addObject:key];
-    [self indent];
-    [data appendFormat:@"%@ = %f;\n", key, value];
-    [keyStack removeLastObject];
+    @synchronized(self) {
+        [keyStack addObject:key];
+        [self indent];
+        [data appendFormat:@"%@ = %f;\n", key, value];
+        [keyStack removeLastObject];
+    }
 }
 
 - (void) encodePoint:(id<XSPoint>)point forKey:(NSString *)key {
-    [self encodeObject:point forKey:key];
+    @synchronized(self) {
+        [self encodeObject:point forKey:key];
+    }
 }
 
 - (void) encodeInteger:(NSInteger)value {
-    //Why did I do that?? (remove if I give up on that)
-//    [self up];
-//    [self indent];
-    [data appendFormat:@"%d"];
-//    [self down];
+    @synchronized(self) {
+        [data appendFormat:@"%d"];
+    }
 }
 
 - (void) encodeInteger:(NSInteger)value forKey:(NSString *)key {
-    [keyStack addObject:key];
-    [self indent];
-    [data appendFormat:@"%@ = %d;\n", key, value];
-    [keyStack removeLastObject];
+    @synchronized(self) {
+        [keyStack addObject:key];
+        [self indent];
+        [data appendFormat:@"%@ = %d;\n", key, value];
+        [keyStack removeLastObject];
+    }
 }
 
 - (void) encodeNilForKey:(NSString *)key {
-    [keyStack addObject:key];
-    [self indent];
-    [data appendFormat:@"%@ = nil;\n", key];
-    [keyStack removeLastObject];
+    @synchronized(self) {
+        [keyStack addObject:key];
+        [self indent];
+        [data appendFormat:@"%@ = nil;\n", key];
+        [keyStack removeLastObject];
+    }
 }
 
 - (void)async:(void (^)(void))block {
@@ -208,6 +234,8 @@
 }
 
 - (void)sync {
-    dispatch_group_wait(disp_group, DISPATCH_TIME_FOREVER);
+    @synchronized(files) {
+        dispatch_group_wait(disp_group, DISPATCH_TIME_FOREVER);
+    }
 }
 @end
